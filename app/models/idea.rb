4ca207b7f7b7704565d3046b815555c606620915
Marko Sanković <sankovic.marko@gmail.com>
2015-06-05 23:49:53 +0200

    Refactor 1

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,12 @@
 class Idea < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
+
+  validates :name, presence: true
+  validates :description, length: {
+    minimum: 20,
+    maximum: 1000,
+    tokenizer: lambda { |str| str.split(/\s+/) },
+    too_short: "must have at least %{count} words",
+    too_long: "must have at most %{count} words"
+  }
 end
